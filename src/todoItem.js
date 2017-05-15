8780270ce13f854e49bfb820b8712f1e772a3439
Johann Kirchkesner <yinzet@web.de>
2017-05-15 18:28:08 +0200

    create component todoItem

---
+++ src/todoItem.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import CheckBox from 'react-native-checkbox';
+
+class TodoItem extends Component{
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    return (
+      <View style={styles.listItem}>
+        <CheckBox
+          label={this.props.label}
+          checked={this.props.checked}
+        />
+        <Text style={styles.categoryStyle}>{this.props.category}</Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  listItem: {
+    flex: 1,
+    padding: 10,
+    flexDirection: 'row',
+    alignSelf: 'stretch',
+  },
+  categoryStyle: {
+    fontSize: 10,
+    color: 'red',
+    margin: 10,
+  }
+});
+
+export default TodoItem;
