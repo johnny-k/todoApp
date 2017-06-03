9591f8d06565ff10499ee602a9939ada348a6e64
Johann Kirchkesner <yinzet@web.de>
2017-06-04 00:30:54 +0200

    style to look like TodoApp

--- index.android.js
+++ index.android.js
@@ -10,29 +10,32 @@ import {
   StyleSheet,
   Text,
   View,
-    AsyncStorage
+  Picker,
 } from 'react-native';
 import TodoView from './src/TodoView';
 import AddView from './src/AddView';
 import ScrollableTabView from 'react-native-scrollable-tab-view';
 
-const DATABASE_KEY = '@TodoStorage:todo';
-
 export default class todoApp extends Component {
 
   constructor(props) {
       super(props);
+
     }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.topTitle}>TODOS</Text>
+        <View style={styles.top}>
+          <Text style={[styles.topElements, styles.topTitle]}>TODOS</Text>
+          <Text style={[styles.topElements, styles.filter]}>...</Text>
+        </View>
         <ScrollableTabView
           tabBarBackgroundColor='#212121'
           tabBarActiveTextColor='white'
           tabBarInactiveTextColor='grey'
-          tabBarUnderlineStyle={styles.tab}>  
+          tabBarUnderlineStyle={styles.tabBar}
+          onChangeTab={(tab) => this.onTabChanged(tab)}>  
           <TodoView tabLabel='TASK LIST' />
           <AddView tabLabel='ADD TASK' />
         </ScrollableTabView>
@@ -40,25 +43,52 @@ export default class todoApp extends Component {
     );
   }
 
+  onTabChanged(tab){
+    if(tab.i !== 0){
+      return;
+    }
+    console.log('changed to tab 0');
+  }
 
+  onChooseFilter(value){
+    console.log('choose filter '+ value);
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
     backgroundColor: '#f5fcff',
   },
-  topTitle:{
-    alignSelf: 'stretch',
+  topElements:{
     color: 'white',
     backgroundColor: '#212121',
     fontSize: 20,
     fontWeight: 'bold',
+    textAlign: 'center',
     padding: 15,
   },
-  tab:{
-    backgroundColor: '#00bcd4'
-  }
+  top:{
+    flexDirection: 'row',
+    alignSelf: 'stretch',
+    backgroundColor: '#212121',
+  },
+  topTitle:{
+    flex: 1,
+    alignSelf: 'flex-start',
+  },
+  filter:{
+    flex: -1,
+    transform: [{rotate: '90deg'}],
+    alignSelf: 'flex-end',
+  },
+  tabBar:{
+    backgroundColor: '#00bcd4',
+    alignSelf: 'flex-start',
+  },
+  
 });
 
 AppRegistry.registerComponent('todoApp', () => todoApp);
